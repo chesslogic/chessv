0377aadd8c11dc5fc07db2369dca156d822ad63b
chesslogic <chesslogicmaster@gmail.com>
2024-12-23 09:12:15 -0800

    upversion

--- APMW.Client/Client.cs
+++ APMW.Client/Client.cs
@@ -150,7 +150,7 @@ namespace Archipelago.APChessV
             "ChecksMate",
             slotName,
             itemsHandlingFlags: ItemsHandlingFlags.AllItems,
-            new Version(0, 2, 11),
+            new Version(0, 2, 12),
             tags: new string[] { "ChecksMate V" },
             password: password,
             requestSlotData: true);
